30d3d46309c0fb3de534e88f24e19e753bf62215
abhya <abhya1559@gmail.com>
2025-10-21 16:03:53 +0530

    initialize project setup with setup.py and requirements.txt; remove main.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mediacl-AI-agent",
+    version="0.1.0",
+    author="Abhyanand",
+    author_email="abhya1559@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
